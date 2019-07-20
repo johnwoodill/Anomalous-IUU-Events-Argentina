db1321c72645f21df2a0c5e0cc95a0871662edd4
Andrew Woodill <johnwoodill@gmail.com>
2019-07-20 10:07:46 -0700

    updating kmeans figures

--- kmeans_animated_plot.R
+++ kmeans_animated_plot.R
@@ -31,27 +31,39 @@ dat$hour <- stringr::str_pad(dat$hour, 2, side = "left", pad = 0)
 dat$min <- stringr::str_pad(dat$min, 2, side = "left", pad = 0)
 
 labels <- dat %>%
-  arrange(year, month, day) %>%
-  group_by(timestamp, cluster) %>%
-  summarise(sum_centers = mean(sum_centers)) %>%
-  group_by(timestamp) %>%
-  arrange(cluster, sum_centers) %>%
+  group_by(year, month, day, hour, cluster) %>%
+  # summarise(sum_centers = mean(sum_centers)) %>%
+  summarise(km_lat = mean(km_lat)) %>%
+  group_by(year, month, day, hour) %>%
+  arrange(km_lat) %>%
   mutate(clusterS = seq_along(cluster)) %>%
+  select(-c(cluster)) %>% 
+  arrange(year, month, day, hour) %>% 
   ungroup()
 
 labels$clusterS <- labels$clusterS - 1
 labels
 
-dat <- left_join(dat, labels, by=c("timestamp"))
+dat <- left_join(dat, labels, by=c("year", "month", "day", "hour", "km_lat"))
 
 #dat <- as.data.frame(read_feather('~/Projects/Anomalous-IUU-Events-Argentina/data/Argentina_5NN_region1_2016-03-01_2016-03-31.feather'))
 
-dat <- select(dat, timestamp, year, month, day, hour, min, lat, lon, km_lat, km_lon, clusterS)
+dat$cluster <- dat$clusterS
+
+# dat <- select(dat, timestamp, year, month, day, hour, min, lat, lon, km_lat, km_lon, cluster)
+
+avg_dist <- dat %>% 
+  group_by(year, month, day, hour, cluster) %>% 
+  summarise(km_avg_dist = round(mean(km_dist), 2))
+
+head(avg_dist)
+
+dat <- left_join(dat, avg_dist, by=c("year", "month", "day", "hour", "cluster"))
+
 
 movdat <- dat
 
 
-#movdat <- filter(movdat, NN <= NN_max)
 movdat <- filter(movdat, month == "03")
 
 #i = movdat$timestamp[100000]
@@ -61,9 +73,8 @@ i = "2016-03-29 00:00:00"
 
 for (i in unique(movdat$timestamp)){
   
-  #subdat <- movdat
   subdat <- filter(movdat, timestamp == i)
-  subdat$clusterS <- factor(subdat$clusterS, levels = c(0, 1, 2, 3), labels = c("0", "1", "2", "3"))
+  subdat$cluster <- factor(subdat$cluster, levels = c(0, 1, 2, 3), labels = c("0", "1", "2", "3"))
   
   date_ <- paste0(subdat$year[1], "-", subdat$month[1], "-", subdat$day[1], " ", subdat$hour[1], ":", subdat$min[1], ":00")
   date_
@@ -75,16 +86,15 @@ for (i in unique(movdat$timestamp)){
     autoplot(bat, geom = c("raster", "contour")) +
     geom_raster(aes(fill=z)) +
     geom_contour(aes(z = z), colour = "white", alpha = 0.05) +
-    scale_fill_gradientn(values = scales::rescale(c(-6600, 0, 1, 1500)),
+    scale_fill_gradientn(values = scales::rescale(c(-6600, 1, 2, 1500)),
                          colors = c("lightsteelblue4", "lightsteelblue2", "#C6E0FC", 
                                     "grey50", "grey70", "grey85")) +
     labs(x=NULL, y=NULL, color="km") +
-    # geom_segment(data=subdat, aes(x=lon, xend=km_lon, y=lat, yend=km_lat, color=cluster), size=0.1) +
-    geom_point(data=subdat, aes(x=lon, y=lat, color=clusterS)) +
-    # geom_point(data=subdat, aes(x=km_lon, y=km_lat), color='red') +
-    
-    # geom_segment(data=subdat, aes(x=vessel_B_lon, xend=vessel_A_lon, y=vessel_B_lat, yend=vessel_A_lat, color=distance), size = 0.1) +
-    # geom_point(data=subdat, aes(x=vessel_A_lon, y=vessel_A_lat), size = .5, color='red', alpha=0.7) +
+    geom_segment(data=subdat, aes(x=lon, xend=km_lon, y=lat, yend=km_lat, color=cluster), size=0.1) +
+    geom_point(data=subdat, aes(x=lon, y=lat, color=cluster)) +
+    geom_point(data=subdat, aes(x=km_lon, y=km_lat), color='red') +
+    geom_text(data=subdat, aes(x=km_lon, y=km_lat, label = km_avg_dist, vjust=-1), size=3.5) +
+    #annotate(data=subdat, "text", x=km_lon, y = km_lat, label=km_avg_dist, size = 4, color='black', fontface=2) +
     annotate("text", x=-54.5, y = -39.25, label=date_, size = 4, color='black', fontface=2) +
     theme(axis.title.x=element_blank(),
           axis.text.x=element_blank(),
@@ -105,7 +115,8 @@ for (i in unique(movdat$timestamp)){
     annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, color = "black", size=1) + # Left
     annotate("segment", x=Inf, xend=Inf, y=-Inf, yend=Inf, color = "black", size=1) + # Right
     annotate("segment", x=-Inf, xend=Inf, y=Inf, yend=Inf, color = "black", size=1) + # Top
-    scale_color_manual(values = c("0" = "blue", "1" = "orange", "2" = "green", "3" = "black")) +
+    # scale_color_manual(values = c("0" = "blue", "1" = "orange", "2" = "green", "3" = "black")) +
+    scale_color_manual(values = c("0" = "#440154FF", "1" = "#31688EFF", "2" = "#35B779FF", "3" = "#FDE725FF")) +
     # scale_y_continuous(expand=c(0,0)) +
     # scale_x_continuous(expand=c(0,0)) +
     NULL
@@ -114,3 +125,4 @@ for (i in unique(movdat$timestamp)){
   print(date_)
   ggsave(filename = paste0("~/Projects/Anomalous-IUU-Events-Argentina/figures/animated_fig/hourly_figs/", date_, ".png"), width = 6, height = 4, plot = movmap)
 }
+
